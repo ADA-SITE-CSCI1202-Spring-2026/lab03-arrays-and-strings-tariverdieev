26b0c691f5b1e65e7117c14310cd44a664ad6cf3
tariverdieev <ftariverdiyev23908@ada.edu.az>
2026-02-19 09:44:27 +0400

    Add files via upload

---
+++ ExplodedString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ExplodedString {
+    public static String explode(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0 ; i<s.length() ; i++){
+            for(int j=0 ; j<=i ; j++){
+                sb.append(s.charAt(j));
+            }
+        }
+        return (sb.toString());
+    }
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      System.out.println(explode(s));
+        
+    }
+}
+
+
